5ec0e13de642106a208d6fd30bda87b58ea560ab
bkhebert <bondman2004tippy@gmail.com>
2025-01-07 13:50:52 -0600

    ADD packagejson, index files, setup mongoose server

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const mongodb = require('mongo')
+const mongoose = require('mongoose')
+
+const dbURI = 'mongodb://localhost:27017/pixitest';
+
+mongoose.connect(dbURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Error connecting to MongoDB:', err));
