9b16c8cd940d078c58afde917c01cde6519f3b52
bkhebert <bondman2004tippy@gmail.com>
2025-01-07 14:05:39 -0600

    ADD organization to files, serve static page

--- server/index.js
+++ server/index.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const app = express();
 
-const port = 4000; 
+const port = 5000; 
+
+// Serve static files from the 'public' folder
+app.use(express.static('pixi-test')); 
+
+// Define a route for the home page
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/pixi-test/index.html');
+});
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
